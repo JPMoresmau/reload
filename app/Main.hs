2afb425ae1f0f64962b969a7e0bfbdf938a8da13
jpmoresmau <jp@moresmau.fr>
2016-06-27 21:05:53 +0200

    pass port as argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
+-- | Executable modul
 module Main where
 
 import           Language.Haskell.Reload (runApp)
 
+import System.Environment
+
+-- | Main entry point
 main :: IO ()
-main = runApp 8081
+main = do
+  -- basic argument handling: the first arg is the port to listen to
+  args <- getArgs
+  let port = case args of
+                (p:_) -> read p
+                _ -> 8080
+  runApp port
